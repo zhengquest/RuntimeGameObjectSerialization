3412a55cc5aa29e2bea72fb70e4343f5aff89865
leon.qu <zhengqu53@hotmail.com>
2021-04-04 11:04:04 +0100

    new classes

---
+++ Assets/Scripts/ExplodeBehaviour.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBehaviour : MonoBehaviour, IBehaviour
+{
+    public string explosionId;
+    public int explosionSize;
+    public float explosionRad;
+}
