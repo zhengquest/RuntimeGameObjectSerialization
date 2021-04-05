5cfaa367aa574ebb9d2c07e60f77d00d53cf5b45
leon.qu <zhengqu53@hotmail.com>
2021-04-05 15:48:50 +0100

    logic to de/serialize a custom entity

--- Assets/Scripts/EntityManager.cs
+++ Assets/Scripts/EntityManager.cs
@@ -16,15 +16,19 @@ public class EntityManager : MonoBehaviour
     public UiManager UiManager;
     
     private string path;
+    private AssetReferenceGameObject chosenInGameObject;
+    private TypeReference chosenInGameBehaviour;
     private Dictionary<TypeReference, JObject> typeJobjectDict;
     
     private void Start()
     {
         path = Path.Combine(Application.persistentDataPath, "Test.txt");
-
+        chosenInGameObject = inGameObjects[0];
+        chosenInGameBehaviour = inGameBehaviours[0];
         CreateTypeToJobjectDict();
-        
-        UiManager.CreateUiForBehaviour(typeJobjectDict);
+
+        UiManager.CreateUiForBehaviours(typeJobjectDict);
+        UiManager.SetupSaveCallback(SaveCustomizedEntity);
     }
 
     private void CreateTypeToJobjectDict()
@@ -51,5 +55,59 @@ public class EntityManager : MonoBehaviour
         return new JObject(jProperties);
     }
 
+    public void SaveCustomizedEntity()
+    {
+        var savedEntityData = new SavedEntity
+        {
+            inGameObject = chosenInGameObject, savedBehaviours = new List<SavedBehaviour>()
+        };
+        
+        foreach (var addedBehaviour in typeJobjectDict.Keys)
+        {
+            savedEntityData.savedBehaviours.Add(new SavedBehaviour
+            {
+                behaviourType = addedBehaviour, behaviourData = typeJobjectDict[addedBehaviour].ToString()
+            });
+        }
+
+        var serializedData = JsonUtility.ToJson(savedEntityData, true);
+        Debug.Log(serializedData);
+        File.WriteAllText(path, serializedData);
+
+        DeserializeSavedEntity();
+    }
+
+    public void DeserializeSavedEntity()
+    {
+        var data = File.ReadAllText(path);
+        var entity = JsonUtility.FromJson(data, typeof(SavedEntity)) as SavedEntity;
+        Debug.Log(entity);
+
+        SpawnEntityAsync(entity);
+    }
+
+    private async void SpawnEntityAsync(SavedEntity savedEntity) 
+    {
+        var go = await savedEntity.inGameObject.InstantiateAsync().Task;
+
+        foreach (var savedBehaviour in savedEntity.savedBehaviours)
+        {
+            var behaviour = go.AddComponent(savedBehaviour.behaviourType);
+            JsonUtility.FromJsonOverwrite(savedBehaviour.behaviourData, behaviour);            
+        }
+    }
 }
 
+[Serializable]
+public class SavedEntity
+{
+    public AssetReferenceGameObject inGameObject;
+    public List<SavedBehaviour> savedBehaviours;
+}
+
+[Serializable]
+public class SavedBehaviour
+{
+    public TypeReference behaviourType;
+    public string behaviourData; //json or pure string or bytes
+}
